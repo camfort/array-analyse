72beb07346f5fcc7438d9f12b613c16fd581db84
Andrew Rice <acr31@cam.ac.uk>
2016-07-03 19:40:58 +0100

    output a default value of 0 for keys not present

--- resultsToTex.py
+++ resultsToTex.py
@@ -2,6 +2,7 @@
 
 import sys
 import re
+from collections import defaultdict
 
 def sanitize(s):
     s = s.strip()
@@ -17,8 +18,8 @@ with open(sys.argv[2]) as f:
         (k,v) = re.match("\\\\newcommand{\\\\(.*)}{(.*)}",l.strip()).groups()
         files[k] = v
 
-corpora = []
-
+corpora = defaultdict(lambda:{})
+keys = {}
 with open(sys.argv[1]) as f:
     for l in f:
         if l.strip():
@@ -26,15 +27,20 @@ with open(sys.argv[1]) as f:
                 corpus = "overall"
             elif l[0:6] == "corpus":
                 corpus = sanitize(l.split(":")[1])
-                corpora.append(corpus)
             else:
                 (key,value) = map(lambda x:x.strip(),l.split(":"))
-                print "\\newcommand{\\%s}{%s}" % (corpus+sanitize(key),value)
+                key = sanitize(key);
+                keys[key] = 1
+                corpora[corpus][key] = 1
+                print "\\newcommand{\\%s}{%s}" % (corpus+key,value)
                 files[sanitize(corpus+key)] = value
 
 (totalTickAssign, totalTickAssignSuccess, totalLoC) = (0,0,0)
-for corpus in corpora:
-    if corpus != "ethreemgmodernise":
+for corpus in corpora.keys():
+    if corpus != "overall" and corpus != "ethreemgmodernise":
+        for key in keys.keys():
+            if not corpora[corpus].has_key(key):
+                print "\\newcommand{\\%s}{0}" % (corpus+key)
         loc = files[corpus+"LoC"]
         tickAssign = files[corpus+"tickAssign"]
         tickAssignSuccess = files[corpus+"tickAssignSuccess"]
@@ -46,3 +52,4 @@ for corpus in corpora:
 
 print "\\newcommand{\\%s}{%.2g}" % ("overalltickAssignPercent", float(totalTickAssign) / float(totalLoC) * 100)
 print "\\newcommand{\\%s}{%.2g}" % ("overalltickAssignSuccessPercent", float(totalTickAssignSuccess) / float(totalLoC) * 100)
+
